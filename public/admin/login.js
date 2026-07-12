7f1b60a1ec0cf17d3ff658c952dae716e67b0185
studiopurpz <studiopurpz@gmail.com>
2026-07-12 13:00:29 +0200

    remove node_modules

--- public/admin/login.js
+++ public/admin/login.js
@@ -1,108 +1,44 @@
-document.getElementById("loginBtn").onclick=async()=>{
-
-
-const username =
-document.getElementById("username").value
-
-
-const password =
-document.getElementById("password").value
-
-
-
-const res = await fetch(
-"/admin-login",
-{
-
-method:"POST",
-
-headers:{
-"Content-Type":"application/json"
-},
-
-body: JSON.stringify({
-    username: username.value,
-    password: password.value
-})
-
-})
-
-
-const data = await res.json()
-
-
-if(data.success){
-
-window.location.href="/admin.html"
-
-}else{
-
-document.getElementById("error").innerText=
-"Zły login lub hasło"
-
-}
-
-
-}
-
 const loginBtn = document.getElementById("loginBtn")
 
 
-loginBtn.onclick = async()=>{
+loginBtn.onclick = async () => {
+
+    const username = 
+    document.getElementById("username").value
 
 
-const username =
-document.getElementById("username").value
+    const password = 
+    document.getElementById("password").value
 
 
-const password =
-document.getElementById("password").value
+    const response = await fetch("/admin-login", {
+
+        method: "POST",
+
+        headers: {
+            "Content-Type": "application/json"
+        },
+
+        body: JSON.stringify({
+            username,
+            password
+        })
+
+    })
 
 
-
-const response = await fetch("/admin-login",{
-
-method:"POST",
-
-headers:{
-"Content-Type":"application/json"
-},
-
-body:JSON.stringify({
-
-username,
-password
-
-})
-
-})
+    const data = await response.json()
 
 
-const data = await response.json()
+    if(data.success){
 
+        window.location.href = "/admin/adminpanel/adminpanel.html"
 
+    } else {
 
-if(data.success){
+        document.getElementById("error").innerText =
+        "Nieprawidłowy login lub hasło"
 
-window.location.href="/admin/adminpanel/adminpanel.html"
-
-}
-
-else{
-
-document.getElementById("error").innerText =
-"Nieprawidłowy login lub hasło"
-
-}
-
-
-}
-
-
-document.getElementById("logout").onclick=async()=>{
-
-await fetch("/admin-logout")
-
-window.location.href="/admin/admin-login.html"
+    }
 
 }
