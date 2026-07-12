0473968fa590d431955466ce701f4a2612b9d7bf
studiopurpz <studiopurpz@gmail.com>
2026-07-12 13:10:10 +0200

    remove node_modules

--- create-admin.js
+++ create-admin.js
@@ -1,21 +1,38 @@
 const sqlite3 = require('sqlite3').verbose()
 const bcrypt = require('bcrypt')
 
+
 const db = new sqlite3.Database('./database.sqlite')
 
-bcrypt.hash('Purpzadmin12!@',10,(err,hash)=>{
 
-  db.run(
-    'INSERT INTO admins(username,password) VALUES(?,?)',
-    ['PURPZ',hash],
-    (err)=>{
-      if(err){
-        console.log(err)
-      }else{
-        console.log('Admin utworzony')
-      }
-      db.close()
-    }
-  )
+const username = "PURPZ"
+const password = "Purpzadmin12!@"
+
+
+bcrypt.hash(password,10,(err,hash)=>{
+
+
+db.run(
+"INSERT INTO admins(username,password) VALUES(?,?)",
+[
+username,
+hash
+],
+(err)=>{
+
+if(err){
+
+console.log(err)
+
+}else{
+
+console.log("Admin dodany")
+
+}
+
+db.close()
+
+})
+
 
 })
